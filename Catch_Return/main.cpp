d7c3c2287099e1fdb55aaaec35357eb7c3c99bf6
Ramon <Ramon@CompTech-Walther.de>
2019-02-10 17:10:52 +0100

    Added

---
+++ Catch_Return/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string Text = "";
+
+    cout << "Text eingeben> ";
+    //cin << Text;
+    getline(cin, Text);
+
+    if (Text == "") {
+        cout << "Error: Die Eingabe war leer!" << endl;
+    } else {
+        cout << "Die Eingabe war '" << Text << "'" << endl;
+    }
+
+    return 0;
+}
